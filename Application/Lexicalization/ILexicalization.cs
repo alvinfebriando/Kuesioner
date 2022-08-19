6c4b19d52a2a36f4f118613030a9228bd3209633
Alvin Febriando <alvin.febriando@outlook.com>
2022-08-19 22:41:13 +0700

    Add Lexicalization class

---
+++ Application/Lexicalization/ILexicalization.cs
@@ -0,0 +1,11 @@
+﻿namespace Kuesioner.Application.Lexicalization;
+
+public interface ILexicalization
+{
+    public string RndResponden();
+    public string RndMendapat();
+    public string RndTertinggi();
+    public string RndTerendah();
+    public string RndNilai();
+    public string GetStatus(double score);
+}
---
+++ Application/Lexicalization/Lexicalization.cs
@@ -0,0 +1,38 @@
+﻿namespace Kuesioner.Application.Lexicalization;
+
+public class Lexicalization : ILexicalization
+{
+    public string RndResponden()
+    {
+        return Util.GetRandom(new List<string> { "mahasiswa", "responden" });
+    }
+
+    public string RndMendapat()
+    {
+        return Util.GetRandom(new List<string> { "mendapat", "menerima", "memiliki", "memperoleh" });
+    }
+
+    public string RndTertinggi()
+    {
+        return Util.GetRandom(new List<string> { "tertinggi", "paling tinggi" });
+    }
+
+    public string RndTerendah()
+    {
+        return Util.GetRandom(new List<string> { "terendah", "paling rendah" });
+    }
+
+    public string RndNilai()
+    {
+        return Util.GetRandom(new List<string> { "nilai", "skor" });
+    }
+
+    public string GetStatus(double score)
+    {
+        if (Math.Round(score) == 1) return "sangat tidak baik";
+        if (Math.Round(score) == 2) return "tidak baik";
+        if (Math.Round(score) == 3) return "cukup";
+        if (Math.Round(score) == 4) return "baik";
+        return "sangat baik";
+    }
+}
