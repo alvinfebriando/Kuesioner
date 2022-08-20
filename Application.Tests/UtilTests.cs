91c702fae89075b367f1d5f06e06ecf0a49111a2
Alvin Febriando <alvin.febriando@outlook.com>
2022-08-20 17:50:42 +0700

    Add Application.Tests project

---
+++ Application.Tests/UtilTests.cs
@@ -0,0 +1,87 @@
+﻿using Kuesioner.Application;
+using Kuesioner.Domain.Entities;
+using Kuesioner.Infrastructure;
+
+namespace Application.Tests;
+
+public class UtilTests
+{
+    [Fact]
+    public void GetRandomShouldReturnOneOfItsMember()
+    {
+        IList<string> lists = new List<string> { "a", "b", "c" };
+        
+        var result = Util.GetRandom(lists);
+        
+        Assert.Contains(result, lists);
+    }
+    [Theory]
+    [MemberData(nameof(TransposeTestData))]
+    public void TransposeShouldTransposeJaggedArray(int[][] arr, int[][]expected)
+    {
+        var result = Util.Transpose(arr);
+        Assert.Equal(expected, result);
+    }
+
+    public static IEnumerable<object[]> TransposeTestData()
+    {
+        var arr = new[]
+        {
+            new[] { 0, 1 },
+            new[] { 2, 3 },
+            new[] { 4, 5 },
+        };
+        var arr2 = new[]
+        {
+            new[] { 0, 1 },
+            new[] { 2, 3 },
+        };
+
+        var transposed = new[]
+        {
+            new[] { 0, 2, 4 },
+            new[] { 1, 3, 5 },
+        };
+        var transposed2 = new[]
+        {
+            new[] { 0, 2 },
+            new[] { 1, 3 },
+        };
+
+        yield return new object[] {arr, transposed};
+        yield return new object[] {arr2, transposed2};
+    }
+    [Theory]
+    [InlineData(6,10)]
+    [InlineData(5,5)]
+    public void GenerateScoreShouldReturnAnRespondentCountXQuestionSizeScoreArray(int question, int respondentCount)
+    {
+        var weights = new[]
+        {
+            Data.Weight["mostly1"],
+            Data.Weight["mostly1"],
+            Data.Weight["mostly1"],
+            Data.Weight["mostly1"],
+            Data.Weight["mostly1"],
+            Data.Weight["mostly1"],
+        };
+        var result = Util.GenerateScore(question, respondentCount, weights);
+        Assert.Equal(respondentCount, result.Length);
+        Assert.Equal(question, result[0].Length);
+        
+    }
+
+    [Fact]
+    public void ProcessShouldReturnAnArrayOfAverageValue()
+    {
+        var answers = new[]
+        {
+            new[] { 1, 3, 5 },
+            new[] { 3, 5, 7 }
+        };
+        var result = Util.Process(answers, Data.Questions);
+        Assert.Equal(2, result[0].Score);
+        Assert.Equal(4, result[1].Score);
+        Assert.Equal(6, result[2].Score);
+    }
+}
