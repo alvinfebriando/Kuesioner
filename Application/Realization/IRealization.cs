26ea7b36abf46ba103bb0109230f1ad339cee74c
Alvin Febriando <alvin.febriando@outlook.com>
2022-08-22 16:23:58 +0700

    Add Realization class

---
+++ Application/Realization/IRealization.cs
@@ -0,0 +1,10 @@
+﻿using Kuesioner.Application.Spec;
+
+namespace Kuesioner.Application.Realization;
+
+public interface IRealization
+{
+    public IEnumerable<ISpec> MPlan { get; set; }
+    public List<string> ConvertToSentence();
+    public string Format();
+}
---
+++ Application/Realization/Realization.cs
@@ -0,0 +1,31 @@
+﻿using Kuesioner.Application.Spec;
+
+namespace Kuesioner.Application.Realization;
+
+public class Realization : IRealization
+{
+    public Realization(IEnumerable<ISpec> mPlan)
+    {
+        MPlan = mPlan;
+    }
+
+    public IEnumerable<ISpec> MPlan { get; set; }
+
+    public string Format()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public List<string> ConvertToSentence()
+    {
+        var output = new List<string>();
+        foreach (var spec in MPlan)
+        {
+            spec.Build();
+            output.AddRange(spec.Sentences);
+        }
+
+        return output;
+    }
+}
